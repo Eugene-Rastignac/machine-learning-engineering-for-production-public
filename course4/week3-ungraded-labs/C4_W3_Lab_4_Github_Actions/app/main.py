f81b9ef86e2b7c231ca92e4c31e51db94613baf6
flefort <lefort.florent@valeuriad.fr>
2022-02-03 14:20:15 +0100

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+# comments for commit
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
